b2644780097055f384fd4b07f1931ff34591445b
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-05 14:01:57 +0330

    add GestureDector to unfocus when tap somewehere else

--- lib/Screens/Login_screen.dart
+++ lib/Screens/Login_screen.dart
@@ -14,76 +14,79 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(Icons.arrow_back)),
-                  const SizedBox(
-                    width: 100,
-                  ),
-                  SizedBox(
-                    height: 80,
-                    width: 80,
-                    child: Image.asset(
-                      'lib/assets/Logo.png',
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.arrow_back)),
+                    const SizedBox(
+                      width: 100,
+                    ),
+                    SizedBox(
                       height: 80,
                       width: 80,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 300,
-                width: 300,
-                child: Image.asset(
-                  'lib/assets/Login.png',
-                  height: 300,
-                  width: 300,
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Form(
-                /* key: _formKey, */
-                child: Column(
-                  children: [
-                    // Other form fields can be added here
-                    PhoneNumberFormField(),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Container(
-                      width: 250,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: const Color.fromRGBO(64, 123, 255, 1),
+                      child: Image.asset(
+                        'lib/assets/Logo.png',
+                        height: 80,
+                        width: 80,
                       ),
-                      child: TextButton(
-                        onPressed: () {
-                          //Navigator.pushNamed(context, );
-                        },
-                        child: const Text(
-                          'وارد شوید',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                    )
+                    ),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 300,
+                  width: 300,
+                  child: Image.asset(
+                    'lib/assets/Login.png',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Form(
+                  /* key: _formKey, */
+                  child: Column(
+                    children: [
+                      // Other form fields can be added here
+                      PhoneNumberFormField(),
+                      const SizedBox(
+                        height: 100,
+                      ),
+                      Container(
+                        width: 250,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: const Color.fromRGBO(64, 123, 255, 1),
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            //Navigator.pushNamed(context, );
+                          },
+                          child: const Text(
+                            'وارد شوید',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
