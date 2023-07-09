bb3ae0e444926fe64f17d4e28aac3cb995a008ec
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-09 18:17:05 +0330

    .

--- lib/Screens/Register_screen.dart
+++ lib/Screens/Register_screen.dart
@@ -24,22 +24,52 @@ class _RegisterState extends State<Register> {
     Provider.of<Auth>(context, listen: false)
         .register(_authData['email']!, _authData['password']!);
   }
-String? validateEmail(String? value) {
-  if (value == null || value.isEmpty) {
-    return 'Please enter your email';
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+
+    // Regular expression pattern for email validation
+    final emailRegex = RegExp(
+      r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$',
+    );
+
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+
+    return null; // Return null if the email is valid
   }
 
-  // Regular expression pattern for email validation
-  final emailRegex = RegExp(
-    r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$',
-  );
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    }
 
-  if (!emailRegex.hasMatch(value)) {
-    return 'Please enter a valid email address';
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter';
+    }
+
+    if (!value.contains(RegExp(r'[a-z]'))) {
+      return 'Password must contain at least one lowercase letter';
+    }
+
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number';
+    }
+
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return 'Password must contain at least one special character';
+    }
+
+    return null; // Return null if the password is valid
   }
 
-  return null; // Return null if the email is valid
-}
   bool isVisible = false;
   Color setbackgroundColor = const Color.fromRGBO(242, 242, 242, 1);
   Color buttonColor = const Color.fromRGBO(64, 123, 255, 1);
@@ -119,14 +149,7 @@ String? validateEmail(String? value) {
                                               Radius.circular(5))),
                                       labelText: 'ایمیل',
                                     ),
-                                    validator: (value) {
-                                      // Phone number validation logic goes here
-                                      if (value == null || value.isEmpty) {
-                                        return 'لطفا ایمیل خود را وارد نمایید';
-                                      }
-                                      // You can add more specific validation rules if needed
-                                      return null;
-                                    },
+                                    validator: validateEmail,
                                     onSaved: (value) {
                                       _authData['email'] = value!;
                                     },
@@ -150,7 +173,7 @@ String? validateEmail(String? value) {
                                               Radius.circular(5))),
                                       labelText: 'رمز عبور',
                                     ),
-                                    validator: validateEmail,
+                                    validator: validatePassword,
                                     onSaved: (value) {
                                       _authData['password'] = value!;
                                     },
