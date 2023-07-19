f9f6afd73a5405b928a07133838d134b5689d8b3
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-19 12:41:06 +0330

    complete the ui of Unit page

--- lib/Screens/Unit.dart
+++ lib/Screens/Unit.dart
@@ -3,62 +3,72 @@ import '../Widgets/Residence.dart';
 import '../Widgets/ResidenceForm.dart';
 
 class Unit extends StatelessWidget {
-  const Unit({super.key});
+  const Unit({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  icon: const Icon(Icons.arrow_back)),
-              const SizedBox(
-                width: 100,
-              ),
-              SizedBox(
-                height: 80,
-                width: 80,
-                child: Image.asset(
-                  'lib/assets/Logo.png',
+                  icon: const Icon(Icons.arrow_back),
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                SizedBox(
                   height: 80,
                   width: 80,
+                  child: Image.asset(
+                    'lib/assets/Logo.png',
+                    height: 80,
+                    width: 80,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Expanded(child: ResidenceList()),
-
-          //BottomNavigationBar(items: )
-        ]),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ResidenceList(),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
-              //shape: Border(left: BorderSide(width: 5)),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25),
-                  topRight: Radius.circular(25),
-                ),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
               ),
-              context: context,
-              builder: (BuildContext context) {
-                return Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    margin: EdgeInsets.all(15),
-                    child: ResidenceForm());
-              });
+            ),
+            context: context,
+            isScrollControlled: true,
+            builder: (BuildContext context) {
+              final screenHeight = MediaQuery.of(context).size.height;
+              final height = screenHeight * 0.8;
+
+              return Container(
+                height: height,
+                margin: const EdgeInsets.all(15),
+                child: const SingleChildScrollView(
+                  child: ResidenceForm(),
+                ),
+              );
+            },
+          );
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/Widgets/ResidenceForm.dart
+++ lib/Widgets/ResidenceForm.dart
@@ -1,89 +1,131 @@
 import 'package:flutter/material.dart';
 
 class ResidenceForm extends StatelessWidget {
-  const ResidenceForm({super.key});
+  const ResidenceForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.7,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const Text(
-              'افزودن ساکن',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(strokeAlign: BorderSide.strokeAlignCenter),
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                  labelText: 'نام مالک',
-                  labelStyle: TextStyle(fontSize: 13)),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              textDirection: TextDirection.rtl,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                    child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            strokeAlign: BorderSide.strokeAlignOutside),
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    labelText: 'واحد',
-                  ),
-                )),
-                SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                    child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            strokeAlign: BorderSide.strokeAlignOutside),
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    labelText: 'طبقه',
-                  ),
-                )),
-                SizedBox(
-                  width: 5,
-                ),
-                Expanded(
-                    child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            strokeAlign: BorderSide.strokeAlignOutside),
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
-                    labelText: 'بلوک',
-                  ),
-                ))
-              ],
-            ),
-            SizedBox(height: 15),
-            Expanded(
-                child: TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(strokeAlign: BorderSide.strokeAlignOutside),
-                    borderRadius: BorderRadius.all(Radius.circular(5))),
-                labelText: 'تعداد پارکینگ',
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                'افزودن ساکن',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
               ),
-            )),
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    labelText: 'نام مالک',
+                    labelStyle: TextStyle(fontSize: 13)),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        labelText: 'واحد',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        labelText: 'طبقه',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        labelText: 'بلوک',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                  textDirection: TextDirection.rtl,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.4, // Set the desired width
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                          labelText: 'تعداد پارکینگ',
+                        ),
+                      ),
+                    ),
+                  ]),
+              const SizedBox(height: 15),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    labelText: 'شماره همراه',
+                    labelStyle: TextStyle()),
+              ),
+              const SizedBox(height: 5),
+              TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    height: 45,
+                    width: 250,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff407bff),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'ثبت ساکن جدید',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
