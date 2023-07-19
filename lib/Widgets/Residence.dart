fc9b494d136c0e546579679db3947306ebfeb605
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-20 00:59:18 +0330

    change this widget to dynamic data provider

--- lib/Widgets/Residence.dart
+++ lib/Widgets/Residence.dart
@@ -1,71 +1,72 @@
 import 'package:flutter/material.dart';
-import './../Provider/Residence_provider.dart';
-import 'package:provider/provider.dart';
 
 class ResidenceList extends StatelessWidget {
-  const ResidenceList({Key? key});
-
+  ResidenceList(
+      {Key? key,
+      required this.id,
+      required this.name,
+      required this.floor,
+      required this.block,
+      required this.unit,
+      this.debt,
+      this.ask, });
+  final String id;
+  final String name;
+  final String floor;
+  final String block;
+  final String unit;
+  String? debt;
+  String? ask;
   @override
   Widget build(BuildContext context) {
-    final listOfResidence = Provider.of<ResidenceProvide>(context);
-    final residenceList = listOfResidence.residenceList;
-
     return Container(
-      height: MediaQuery.of(context).size.height * 0.7,
-      child: ListView.builder(
-        itemCount: residenceList.length,
-        itemBuilder: (context, index) {
-          final residence = residenceList[index];
-
-          return Container(
-            height: 100,
-            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 14),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: const Color(0xff8cb0ff),
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: Container(
+          height: 100,
+          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 14),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: const Color(0xff8cb0ff),
+          ),
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  block,
+                  textDirection: TextDirection.rtl,
+                ),
+                Text(
+                  floor,
+                  textDirection: TextDirection.rtl,
+                ),
+                Text(
+                  unit,
+                  textDirection: TextDirection.rtl,
+                ),
+              ],
             ),
-            child: ListTile(
-              contentPadding: const EdgeInsets.all(5),
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'بلوک: ${residence['block']}',
-                    textDirection: TextDirection.rtl,
-                  ),
-                  Text(
-                    'طبقه: ${residence['floor']}',
-                    textDirection: TextDirection.rtl,
-                  ),
-                  Text(
-                    'واحد: ${residence['unit']}',
-                    textDirection: TextDirection.rtl,
-                  ),
-                ],
-              ),
-              trailing: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text('name'),
-                  Container(
-                    margin: const EdgeInsets.only(left: 70),
-                    child: const CircleAvatar(
-                      backgroundColor: Color.fromRGBO(206, 221, 255, 1),
-                      child: Icon(
-                        Icons.person,
-                        color: Color.fromRGBO(64, 123, 255, 1),
-                      ),
+            trailing: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(name),
+                Container(
+                  margin: const EdgeInsets.only(left: 70),
+                  child: const CircleAvatar(
+                    backgroundColor: Color.fromRGBO(206, 221, 255, 1),
+                    child: Icon(
+                      Icons.person,
+                      color: Color.fromRGBO(64, 123, 255, 1),
                     ),
                   ),
-                ],
-              ),
-              leading: Text('بدهی/طلب:' /* ${residence.debt} */),
+                ),
+              ],
             ),
-          );
-        },
-      ),
-    );
+            leading: Text('بدهی/طلب:' /* ${residence.debt} */),
+          ),
+        ));
   }
 }
