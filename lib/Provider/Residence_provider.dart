919b72c94863906f47a6e0909580384768eeb0f8
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-23 17:17:14 +0330

    insert data to database

--- lib/Provider/Residence_provider.dart
+++ lib/Provider/Residence_provider.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:supabase/supabase.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
 
 class ResidenceModel extends ChangeNotifier {
-  final String id;
-  final String name;
-  final String unit;
-  final String floor;
-  final String block;
-  final int parking_numbers;
-  final String phone;
-  final String? debt;
-  final String? request;
+  String id;
+  String name;
+  String unit;
+  String floor;
+  String block;
+  String parking_numbers;
+  String phone;
+  String? debt;
+  String? request;
 
   ResidenceModel({
     required this.id,
@@ -33,26 +35,37 @@ class ResidenceProvider extends ChangeNotifier {
   List<ResidenceModel> get residenceList => [..._residenceList];
   List<ResidenceModel> _residenceList = [];
 
-  Future<void> addResidence(ResidenceModel residence) async {
-    await supabase.from('residence').insert({
-      'id': residence.id,
-      'block': residence.block,
-      'unit': residence.unit,
-      'phone_number': residence.phone,
-      'floor': residence.floor,
-      'name': residence.name,
-      'number_of_parking': residence.parking_numbers
-    }).then((value) {
-      final newresidence = ResidenceModel(
-          id: residence.id,
-          name: residence.name,
-          unit: residence.unit,
-          floor: residence.floor,
-          block: residence.block,
-          parking_numbers: residence.parking_numbers,
-          phone: residence.phone);
-      residenceList.add(newresidence);
-      notifyListeners();
-    });
+  Future<void> addResidence(String name, String block, String unit,
+      String phone, String floor, String parking) async {
+    try {
+      var uuid = Uuid();
+
+      // Generate a v1 (time-based) id
+      var iduuid = uuid.v1();
+      var id = iduuid;
+      final response = await supabase.from('residence').insert({
+        'id': id,
+        'block': block,
+        'unit': unit,
+        'phone_number': phone,
+        'floor': floor,
+        'name_of_Owner': name,
+        'number_of_parking': parking
+      }).then((value) {
+        print('its ok');
+        final newresidence = ResidenceModel(
+            id: id,
+            name: name,
+            unit: unit,
+            floor: floor,
+            block: block,
+            parking_numbers: parking,
+            phone: phone);
+        residenceList.add(newresidence);
+        notifyListeners();
+      });
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
