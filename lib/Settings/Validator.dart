06e0966496dc135a5f397d01ef50ca6db8c79b52
RexGod <shadmehrmanoochehri@gmail.com>
2023-07-24 16:45:21 +0330

    Add validator for phone number

--- lib/Settings/Validator.dart
+++ lib/Settings/Validator.dart
@@ -3,26 +3,24 @@ import 'package:flutter/foundation.dart';
 class Setting with ChangeNotifier {
   String? validatePassword(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Please enter a password';
+      return 'رمز عبور خود را وارد نمایید';
     }
     if (value.length < 8) {
-      return 'Password must be at least 8 characters long';
+      return 'حداقل 8 کارکتر باشد';
     }
-
-    if (!value.contains(RegExp(r'[A-Z]'))) {
-      return 'Password must contain at least one uppercase letter';
-    }
-
     if (!value.contains(RegExp(r'[a-z]'))) {
-      return 'Password must contain at least one lowercase letter';
+      return 'حداقل شامل یک حرف بزرگ باشد';
+    }
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'حداقل شامل یک حرف کوچک باشد';
     }
 
     if (!value.contains(RegExp(r'[0-9]'))) {
-      return 'Password must contain at least one number';
+      return 'شامل یک عدد حداقل باشد';
     }
 
     if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-      return 'Password must contain at least one special character';
+      return 'حداقل شامل یک کارکتر خاص باشد';
     }
 
     return null; // Return null if the password is valid
@@ -30,7 +28,7 @@ class Setting with ChangeNotifier {
 
   String? validateEmail(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Please enter your email';
+      return 'لطفا ایمیل خود را وارد نمایید';
     }
 
     // Regular expression pattern for email validation
@@ -39,9 +37,21 @@ class Setting with ChangeNotifier {
     );
 
     if (!emailRegex.hasMatch(value)) {
-      return 'Please enter a valid email address';
+      return 'لطفا ایمیل معتبر وارد کنید';
     }
 
     return null; // Return null if the email is valid
+  }
+
+  String? validphoneNumber(String? value) {
+    if (value == null) {
+      return 'لطفا شماره را وارد نمایید';
+    }
+
+    final iranPhoneRegex = r'^09[0-9]{9}$|^0[1-9][0-9]{7}$';
+    if (!RegExp(iranPhoneRegex).hasMatch(value)) {
+      return 'لطفا شماره معتبری وارد نمایید';
+    }
+    return null; // Return null if the input is valid
   }
 }
