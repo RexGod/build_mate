e9f54630555d1068e118225de42f7af45a9d1eca
Shadmehr <shadmehrmanoochehri@gmail.com>
2023-08-05 00:08:22 +0330

    costitem Ui

--- lib/Widgets/costItem.dart
+++ lib/Widgets/costItem.dart
@@ -6,34 +6,43 @@ class CostItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
-      height: MediaQuery.of(context).size.height * 0.14,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color.fromRGBO(63, 155, 242, 1.0), width: 2),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(5),
+          topRight: Radius.circular(5),
+        ),
+      ),
+      margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+      height: MediaQuery.of(context).size.height * 0.15,
       child: Column(
         children: [
           Directionality(
             textDirection: TextDirection.rtl,
             child: Container(
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 63, 155, 242),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(5),
-                  topRight: Radius.circular(5),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Color.fromRGBO(63, 155, 242, 1.0)),
                 ),
               ),
-              padding: const EdgeInsets.symmetric(horizontal: 15),
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Text(
                     'نوع هزینه',
                     style: TextStyle(
-                        fontSize: 16,
-                        fontWeight:
-                            FontWeight.bold), // Bold and change font size here
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(
+                          63, 155, 242, 1.0), // Bold and change font size here
+                    ),
                   ),
                   IconButton(
                     onPressed: () {},
-                    icon: const Icon(Icons.delete_forever, size: 35),
+                    icon: const Icon(Icons.delete_forever,
+                        size: 35,
+                        color: Colors.red), // Use red color for delete icon
                   )
                 ],
               ),
@@ -42,8 +51,7 @@ class CostItem extends StatelessWidget {
           Directionality(
             textDirection: TextDirection.rtl,
             child: Container(
-              color: const Color.fromARGB(146, 49, 141, 227),
-              padding: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -52,17 +60,25 @@ class CostItem extends StatelessWidget {
                       Text(
                         'قیمت',
                         style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight
-                                .bold), // Bold and change font size here
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(63, 155, 242,
+                              1.0), // Bold and change font size here
+                        ),
                       ),
                       Text(
                         ' 150000',
-                        style: TextStyle(fontSize: 16), // Change font size here
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromRGBO(
+                                113, 166, 96, 1.0)), // Use green for price
                       ),
                       Text(
                         'تومان',
-                        style: TextStyle(fontSize: 14), // Change font size here
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(
+                                128, 128, 128, 1.0)), // Use grey for currency
                       ),
                     ],
                   ),
@@ -71,17 +87,24 @@ class CostItem extends StatelessWidget {
                       Text(
                         'تاریخ',
                         style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight
-                                .bold), // Bold and change font size here
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(63, 155, 242,
+                              1.0), // Bold and change font size here
+                        ),
                       ),
                       Text(
                         ' 1402/05/02',
-                        style: TextStyle(fontSize: 16), // Change font size here
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black), // Use black for date
                       ),
                       Text(
                         'چهارشنبه',
-                        style: TextStyle(fontSize: 14), // Change font size here
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(
+                                128, 128, 128, 1.0)), // Use grey for day
                       ),
                     ],
                   ),
@@ -94,9 +117,9 @@ class CostItem extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20),
                             ),
                           ),
-                          backgroundColor: const MaterialStatePropertyAll(
-                            Color.fromARGB(255, 113, 166, 96),
-                          ),
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromRGBO(63, 155, 242,
+                                  1.0)), // Use green for button background
                         ),
                         onPressed: () {},
                         child: const Padding(
@@ -104,9 +127,10 @@ class CostItem extends StatelessWidget {
                           child: Text(
                             'پرداخت',
                             style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight
-                                    .bold), // Bold and change font size here
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white, // Use white for button text
+                            ),
                           ),
                         ),
                       ),
@@ -114,14 +138,16 @@ class CostItem extends StatelessWidget {
                         onPressed: () {},
                         icon: const Icon(
                           Icons.cancel_outlined,
-                          color: Color.fromARGB(255, 0, 27, 114),
+                          color: Color.fromRGBO(
+                              0, 27, 114, 1.0), // Use blue for the icon
                         ),
                         label: const Text(
                           'پرداخت نشده',
                           style: TextStyle(
-                            fontSize: 14, // Change font size here
-                            fontWeight: FontWeight.bold, // Bold
-                            color: Color.fromARGB(255, 0, 27, 114),
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(
+                                0, 27, 114, 1.0), // Use blue for the text
                           ),
                         ),
                       ),
