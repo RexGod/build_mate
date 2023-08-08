712a6991d30d0433a48ccc985a1d661626b01745
Shadmehr <shadmehrmanoochehri@gmail.com>
2023-08-08 17:31:25 +0330

    add a method to update price

--- lib/Provider/cost_Provider.dart
+++ lib/Provider/cost_Provider.dart
@@ -6,19 +6,13 @@ import 'package:shamsi_date/shamsi_date.dart';
 import 'package:supabase/supabase.dart';
 import 'package:intl/intl.dart';
 
-
 class ProviderCost with ChangeNotifier {
   SupabaseClient supabase = SupabaseClient(
       'https://vzlhnipbllxwusreekcb.supabase.co',
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6bGhuaXBibGx4d3VzcmVla2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzMzY5NTYsImV4cCI6MTk5NzkxMjk1Nn0.Sn9FWq3SB_wwV76niREsmrL9bBDzEsEPusVW-9TG3So');
 
   // ignore: non_constant_identifier_names
-  Future<void> InsertCost(
-      /* String type, DateTime date, String price */ CostModel
-          costModel) async {
-    /* var iduuid = Uuid();
-    var id = int.parse(iduuid.v1()); */
-
+  Future<void> InsertCost(CostModel costModel) async {
     costModel.date = DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd').format(costModel.date);
     String dayOfWeek = DateFormat('EEEE').format(costModel.date);
@@ -28,10 +22,12 @@ class ProviderCost with ChangeNotifier {
         'price': costModel.price,
         'date': formattedDate,
         'day': dayOfWeek,
+        'remainingPrice': costModel.price
         /* 'id': id */
       });
-    // ignore: empty_catches
+      // ignore: empty_catches
     } catch (error) {
+      print(error.toString());
     }
     notifyListeners();
   }
@@ -39,10 +35,14 @@ class ProviderCost with ChangeNotifier {
   List<dynamic> _costItems = [];
 
   Future<void> fetchCostItem() async {
-    final response = await supabase
-        .from('payment')
-        .select('''id , price, day, date, status, type''');
-    _costItems = response;
+    try {
+      final response = await supabase
+          .from('payment')
+          .select('''id , price, day, date, status, type , remainingPrice''');
+      _costItems = response;
+    } catch (error) {
+      print(error.toString());
+    }
 
     notifyListeners();
   }
@@ -57,14 +57,16 @@ class ProviderCost with ChangeNotifier {
     }
     return totalCost;
   }
-
+  
   Future<void> deleteCostItem(int id) async {
     try {
       await supabase.from('payment').delete().eq('id', id);
-    // ignore: empty_catches
+      // ignore: empty_catches
     } catch (error) {
+      print(error.toString());
     }
   }
+
   String format1(Date d) {
     final f = d.formatter;
 
@@ -76,10 +78,15 @@ class ProviderCost with ChangeNotifier {
 
     return f.wN;
   }
-  Future<void> updateStatus(int id , bool status) async {
-    await supabase.from('payment').update({'status' : true}).match({'id' : id});
+
+  Future<void> updateStatus(int id, bool status) async {
+    await supabase.from('payment').update({'status': true}).match({'id': id});
   }
 
-
-
+  Future<void> updateprice(int id, double price, double remainingPrice) async {
+    remainingPrice = remainingPrice - price;
+    await supabase
+        .from('payment')
+        .update({'remainingPrice': remainingPrice}).match({'id': id});
+  }
 }
